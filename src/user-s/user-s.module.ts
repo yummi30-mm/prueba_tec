621153926033f37399cbc67ddfdd6c217e9aadaf
naysha30 <mayumiarroyotejeda@gmail.com>
2026-01-16 13:48:31 -0500

    fix: correct relative import paths in UserSModule

--- src/user-s/user-s.module.ts
+++ src/user-s/user-s.module.ts
@@ -1,4 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../users/entities/user.entity';
+import { UsersService } from '../users/users.service';
+import { UsersController } from '../users/users.controller';
 
-@Module({})
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([User]),
+  ],
+  providers: [UsersService],
+  controllers: [UsersController],
+  exports: [UsersService],
+})
 export class UserSModule {}
